013ed5a7d55e09ceb94b6bd573d31d9e8a065048
pwegrzynowicz <patrycja@yonlabs.com>
2019-03-20 23:55:07 +0100

    print/add logic added.

--- ocp/main.cpp
+++ ocp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <sstream>
 
 // A simple parser.
 int main() {
@@ -18,10 +19,33 @@ int main() {
             }
             case '!': {
                 // Process the command in this line.
-                if (line.find('!print') == 0) {
-                    // print command
-                } else if (line.find('!add') == 0) {
-                    // add command
+                if (line.find("!print", 0) == 0) {
+                    // Process the print command.
+                    std::istringstream iss(line);
+                    std::vector<std::string> tokens(std::istream_iterator<std::string>{iss},
+                                                    std::istream_iterator<std::string>());
+                    auto it = tokens.begin();
+                    it++;
+                    while (it != tokens.end()) {
+                        std::string token = *it++;
+                        if (token[0] == '$') {
+                            std::cout << variables[token];
+                        } else {
+                            std::cout << token;
+                        }
+                    }
+                    std::cout << std::endl;
+
+                } else if (line.find("!add", 0) == 0) {
+                    // Process the add command: add another variable or value to a specified variable.
+                    std::istringstream iss(line);
+                    std::vector<std::string> tokens(std::istream_iterator<std::string>{iss},
+                                                    std::istream_iterator<std::string>());
+                    if (tokens.size() == 3 and tokens[1][0] == '$') {
+                        std::string variableName = tokens[1];
+                        std::string arg = (tokens[2][0] == '$') ? variables[tokens[2]] : tokens[2];
+                        variables[variableName] += arg;
+                    }
                 }
                 break;
             }
