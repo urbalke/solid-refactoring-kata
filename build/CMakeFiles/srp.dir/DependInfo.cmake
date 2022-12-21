4741e983390fdec469493e8dedfd74e53ff958a7
urba <patryk.urba@nokia.com>
2022-12-21 13:46:31 +0100

    pushing

---
+++ build/CMakeFiles/srp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/urba/szkolenie/solid-refactoring-kata/srp/DbService.cpp" "CMakeFiles/srp.dir/DbService.cpp.o" "gcc" "CMakeFiles/srp.dir/DbService.cpp.o.d"
+  "/home/urba/szkolenie/solid-refactoring-kata/srp/SmtpClient.cpp" "CMakeFiles/srp.dir/SmtpClient.cpp.o" "gcc" "CMakeFiles/srp.dir/SmtpClient.cpp.o.d"
+  "/home/urba/szkolenie/solid-refactoring-kata/srp/User.cpp" "CMakeFiles/srp.dir/User.cpp.o" "gcc" "CMakeFiles/srp.dir/User.cpp.o.d"
+  "/home/urba/szkolenie/solid-refactoring-kata/srp/UserManager.cpp" "CMakeFiles/srp.dir/UserManager.cpp.o" "gcc" "CMakeFiles/srp.dir/UserManager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/srp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/srp.dir/DbService.cpp.o"
+  "CMakeFiles/srp.dir/DbService.cpp.o.d"
+  "CMakeFiles/srp.dir/SmtpClient.cpp.o"
+  "CMakeFiles/srp.dir/SmtpClient.cpp.o.d"
+  "CMakeFiles/srp.dir/User.cpp.o"
+  "CMakeFiles/srp.dir/User.cpp.o.d"
+  "CMakeFiles/srp.dir/UserManager.cpp.o"
+  "CMakeFiles/srp.dir/UserManager.cpp.o.d"
+  "libsrp.a"
+  "libsrp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
